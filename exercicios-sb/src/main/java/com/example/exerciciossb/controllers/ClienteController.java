76b6d94221fc6c8f350f7c71cc8133548b92f455
Iaquine <61883817+Iaquine@users.noreply.github.com>
2022-01-03 12:12:54 -0300

    Refactored controllers

--- exercicios-sb/src/main/java/com/example/exerciciossb/controllers/ClienteController.java
+++ exercicios-sb/src/main/java/com/example/exerciciossb/controllers/ClienteController.java
@@ -1,14 +1,16 @@
 package com.example.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.exerciciossb.models.Cliente;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
-	@GetMapping(path = "/clientes")
+	@GetMapping(path = "/primeiro")
 	public Cliente obterCliente() {
 		return new Cliente(28, "Pedro", "123.456.789-00");
 	}
