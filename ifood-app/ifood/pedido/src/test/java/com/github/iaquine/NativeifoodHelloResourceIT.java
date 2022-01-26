82e785d001fb79599421952b3887029f5e2488fd
Iaquine <61883817+Iaquine@users.noreply.github.com>
2022-01-26 14:32:46 -0300

    Starting "ifood" project
    Created project;
    Modules configuration;
    docker-compose.yml file configuration

---
+++ ifood-app/ifood/pedido/src/test/java/com/github/iaquine/NativeifoodHelloResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.iaquine;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeifoodHelloResourceIT extends ifoodHelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
