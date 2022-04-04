3b74ec366907a48f0ad3d4509a80ca8c8765bbee
Iaquine <61883817+Iaquine@users.noreply.github.com>
2022-04-04 11:51:34 -0300

    Deleting a project

--- ifood-app/ifood/pedido/src/test/java/com/github/iaquine/NativeifoodHelloResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.github.iaquine;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeifoodHelloResourceIT extends ifoodHelloResourceTest {
-
-    // Execute the same tests but in native mode.
-}
